4b357cd499d17b84ef28722c0963ce9394074b03
sbtestproj <fomenko.dg@yandex.ru>
2019-07-31 14:25:26 +0300

    table-configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule, MatTreeModule} from '@angular/material';
+import { TableTestComponent } from './table-test/table-test.component';
+import {MatPaginatorModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination' ;
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { TableConfigsComponent } from './table-configs/table-configs.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TableTestComponent,
+    TableConfigsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, Ng2SearchPipeModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatTreeModule,
+    HttpClientModule,
+    NgxPaginationModule, NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
