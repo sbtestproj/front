59a23fdb1e5782b6f158672ce494991c414fc06d
Bora SAKTANBER <boraks0135@hotmail.com>
2019-08-23 13:42:10 +0200

    third-step added

---
+++ src/app/third-step/third-step.component.ts
@@ -0,0 +1,64 @@
+import {Component, Injectable, OnInit} from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
+import {possible} from '../Models/possibleValues';
+import {AddparamwindowComponent} from '../addparamwindow/addparamwindow.component';
+
+@Component({
+  selector: 'app-third-step',
+  templateUrl: './third-step.component.html',
+  styleUrls: ['./third-step.component.css']
+})
+@Injectable({
+  providedIn: 'root'
+})
+export class ThirdStepComponent implements OnInit {
+  registrationForm: FormGroup;
+  public testResult: possible;
+  constructor(
+    private fb: FormBuilder,
+    public main: AddparamwindowComponent
+  ) {
+  }
+
+  /**
+   * Form initialization
+   */
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      possibleValues: this.fb.array([
+        this.getUnit()
+      ])
+    });
+    this.main.secondFormGroup = this.registrationForm ;
+    this.registrationForm.statusChanges.subscribe(st => {this.main.thirdFormGroup = this.registrationForm; } );
+  }
+  get possibleValues() {
+    return this.registrationForm.get('possibleValues') as FormArray;
+  }
+  deletePossibleValues(i: number) {
+      this.possibleValues.removeAt(i);
+      // if (i === 0 && this.possibleValues.length === 0) {
+      //   this.addPossibleValues();
+      // }
+  }
+
+  addPossibleValues() {
+    if (this.registrationForm.valid) {
+      this.possibleValues.push(this.getUnit());
+    }
+  }
+  private getUnit() {
+    return this.fb.group({
+      newValue: ['', Validators.required],
+      newDescription: ['', Validators.required],
+    });
+  }
+
+  save() {
+    this.testResult = new class implements possible {
+      possibleValues: [{ newValue: undefined; newDescription: undefined }];
+    }
+    this.testResult = this.registrationForm.value;
+    this.testResult.possibleValues.forEach(x => console.log('/n description ==' + x.newDescription + '/n value ==' + x.newValue));
+  }
+}
